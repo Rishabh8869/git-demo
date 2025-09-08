2ebc0d2fc38c4560194aec41b94cf040bd3beb03
Rishabh8869 <rishabhsingh202020@gmail.com>
2025-09-08 15:45:48 +0530

    fixed uy esu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,9 @@
+
 import React from 'react'
 import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom';
 
-
-
-
 const Navbar = (props) => {
-
   return (
     <div>
       <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} `}>
@@ -20,47 +17,31 @@ const Navbar = (props) => {
               <li className="nav-item">
                 <Link className="nav-link active" aria-current="page" to="/">Home</Link>
               </li>
-              {/* <li className="nav-item">
-                <a className="nav-link" href="/">Features</a>
-              </li> */}
-               <li className="nav-item">
+              <li className="nav-item">
                 <Link className="nav-link active" aria-current="page" to="/about">About</Link>
               </li>
-              {/* <li className="nav-item">
-                <Link className="nav-link" to="/about">{props.aboutText}</Link>
-              </li> */}
-              {/* <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-              </li> */}
             </ul>
-            {/* <form className="d-flex ml-4">
-              <input className="form-control me-2" type="search" placeholder="search" aria-label="search"  />
-              <button className="btn btn-primary" type="submit">Search</button>
-            </form> */}
-            <div className="d-flex">
-              <div className="bg-primary rounded mx-2" style={{height:'30px',width:'30px'}}></div>
+
+            {/* Dark mode switch */}
+            <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+              <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="switchCheckDefault"/>
+              <label className="form-check-label" htmlFor="switchCheckDefault">Enable Darkmode</label>
             </div>
-            <div className={`form-check form-switch text-${props.mode ==='light'?'dark':'light'}`}>   {/* it is sad that if the props mode is light than it change into dark and dark is change into light*/}
-                 <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="switchCheckDefault"/>
-                 <label className="form-check-label" htmlFor="switchCheckDefault">Enable Darkmode</label>
-            </div>
-             
           </div>
         </div>
       </nav>
     </div>
   )
-
 }
 
 export default Navbar
 
 Navbar.propTypes = {
-    title:PropTypes.string,
-abouttext: PropTypes.string
+  title: PropTypes.string,
+  abouttext: PropTypes.string
 }
 
 Navbar.defaultProps = {
-    title: 'Set title here',
-    aboutText : 'About text here'
+  title: 'Set title here',
+  aboutText : 'About text here'
 };
